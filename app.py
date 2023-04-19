4ceb8f267bb8b45bbac87a6aa734f2c158ea6f01
edytaroz <edytaroz11@poczta.fm>
2023-04-19 16:42:23 +0200

    Init

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sklearn.metrics import accuracy_score
 import openX
 
 app = Flask(__name__)
@@ -20,10 +21,10 @@ def choice():
         elif title == "o2":
             data = openX.knnclassifier(X_train, X_test, y_train, y_test)
             data2 = openX.dtclassifier(X_train, X_test, y_train, y_test)
-            return "K nearest neighbors accuracy: " + str(data) + "\n" + "Decision tree accuracy: " + str(data2)
+            return "K nearest neighbors accuracy: " + str(accuracy_score(y_test,data)) + "\n" + "Decision tree accuracy: " + str(accuracy_score(y_test,data2))
         elif title == "o3":
             data = openX.nnclassifier(X_train, X_test, y_train, y_test)
-    return "Accuracy: " + str(data)
+        return "Accuracy: " + str(accuracy_score(y_test,data))
 
 
 if __name__ == '__main__':
--- openX.py
+++ openX.py
@@ -1,8 +1,7 @@
 import random
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -19,21 +18,21 @@ def preprocess():
 
 def heuristic(y_test):
     res = np.array([random.randrange(1,7,1) for _ in range(len(y_test))])
-    return accuracy_score(y_test,res)
+    return res
 
 
 def knnclassifier(X_train, X_test, y_train, y_test):
     knn = KNeighborsClassifier(algorithm='kd_tree')
     knn.fit(X_train,y_train)
     y_pred = knn.predict(X_test)
-    return accuracy_score(y_test,y_pred)
+    return y_pred
 
 
 def dtclassifier(X_train, X_test, y_train, y_test):
     dc = DecisionTreeClassifier()
     dc.fit(X_train,y_train)
     dcpred = dc.predict(X_test)
-    return accuracy_score(y_test,dcpred)
+    return dcpred
 
 
 def nnclassifier(X_train,X_test,y_train,y_test):
@@ -43,6 +42,9 @@ def nnclassifier(X_train,X_test,y_train,y_test):
     outputs = tf.keras.layers.Dense(10, activation="softmax", name="predictions")(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model1 = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model2 = tf.keras.Model(inputs=inputs, outputs=outputs)
+
     X_train = X_train.astype("float32")
     X_test = X_test.astype("float32")
     y_train = y_train.astype("float32")
@@ -57,8 +59,31 @@ def nnclassifier(X_train,X_test,y_train,y_test):
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
     )
-
+    model1.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.MeanSquaredError(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+    model2.compile(
+        optimizer=tf.keras.optimizers.RMSprop(),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
     history = model.fit(
+        X_train,
+        y_train,
+        batch_size=256,
+        epochs=15,
+        validation_data=(x_val, y_val),
+    )
+    history1 = model1.fit(
+        X_train,
+        y_train,
+        batch_size=256,
+        epochs=15,
+        validation_data=(x_val, y_val),
+    )
+    history2 = model2.fit(
         X_train,
         y_train,
         batch_size=256,
@@ -67,5 +92,27 @@ def nnclassifier(X_train,X_test,y_train,y_test):
     )
 
     results = model.evaluate(X_test, y_test, batch_size=128)
+    results1 = model1.evaluate(X_test, y_test, batch_size=128)
+    results2 = model2.evaluate(X_test, y_test, batch_size=128)
+    if results2[1] >= results1[1] and results2[1] >= results[1]:
+        loss = history2.history2['loss']
+        val_loss = history2.history2['val_loss']
+        plt.plot(loss)
+        plt.plot(val_loss)
+        plt.show()
+        return results2[1]
+    elif results1[1] >= results2[1] and results1[1] >= results[1]:
+        loss = history1.history1['loss']
+        val_loss = history1.history1['val_loss']
+        plt.plot(loss)
+        plt.plot(val_loss)
+        plt.show()
+        return results1[1]
+    else:
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        plt.plot(loss)
+        plt.plot(val_loss)
+        plt.show()
+        return results[1]
     # print("test loss, test acc:", results)
-    return results[1]
